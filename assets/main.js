e5adfc0d4cc75e55981c8450410a704676e172bf
sarinaem <sarinaem75@gmail.com>
2023-11-08 23:42:23 +0330

    add two part of page like 2 box

--- assets/main.js
+++ assets/main.js
@@ -21,6 +21,39 @@ icon.addEventListener ('mouseover', function(){
     icon.classList.add ('fa-arrow-right');
 });
 
+const text = document.getElementById ("typeWriter");
+const words = [" " , "members", "hackers", "learners" , ""];
+let wordIndex =0;
+let index = 0;
+let showCursor = true;
+function textload(){
+    text.textContent = words[wordIndex].substring(0, index);
+    if (index > words[wordIndex].length){
+        console.log("inside if", index, words[wordIndex]);
+
+        wordIndex +=1;
+        if (wordIndex === words.length){
+            wordIndex = 0;
+        }
+        index = 0;
+        text.textContent = "";
+    }
+    index++;
+    const timer = setTimeout(textload, 500)
+}
+textload();
+
+
+
+setInterval(()=>{
+    if (showCursor){
+        text.style.borderRight = "";
+        showCursor = false;
+    } else {
+        text.style.borderRight = "2px solid #9FEF00";
+        showCursor = true;
+    }
+}, 300)
 
 
 
